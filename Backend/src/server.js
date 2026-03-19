78205bb4dda3b51432a72085be8dfccaf3af966a
Ayush6950 <ayushmalviya9294@gmail.com>
2026-03-19 14:13:18 +0530

    initial commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,19 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import messageroutes from './routes/message.routes.js';
+import path from 'path';
 
+import messageroutes from './routes/message.routes.js';
 import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve(); // ✅ fix name
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageroutes);
-app.listen(PORT,() =>{
-    console.log ("Server is running on port " + PORT)
-});
 
-    
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); // ✅ Vite uses dist
+
+    // ✅ FIX: remove app.get("/*") and use this
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+});
