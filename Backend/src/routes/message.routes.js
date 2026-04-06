1975ac5bd021df87a2aaf0007d842ccd430bcb32
Ayush6950 <ayushmalviya9294@gmail.com>
2026-04-06 22:58:36 +0530

    message routes

--- Backend/src/routes/message.routes.js
+++ Backend/src/routes/message.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+router.use(arcjetProtection);
 
-router.get('/send' , (res, req) => {
-    res.send("send message endpoint");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-export default router;
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+
+export default router;  
