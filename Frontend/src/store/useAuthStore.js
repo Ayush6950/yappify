43b93e2455f8467b4d7cd9cced71e4222e9b9a93
Ayush6950 <ayushmalviya9294@gmail.com>
2026-08-11 14:00:35 +0530

    deployment-front

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -8,7 +8,7 @@ import { useChatStore } from "./useChatStore";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "/";
+    : import.meta.env.VITE_API_URL;
 
 export const useAuthStore = create((set, get) => ({
   // =========================
